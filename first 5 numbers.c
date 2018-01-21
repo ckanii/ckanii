5f62c06fd5fdcf0ce571b26260525bf86dd2d49d
ckanii <35136096+ckanii@users.noreply.github.com>
2018-01-21 05:48:44 +0000

    Create first 5 numbers.c

---
+++ first 5 numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+int sum=0,i,n;
+clrscr();
+printf("\n enter the value of n:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("\n 5* %d=%d",i,5*i);
+}
+getch();
+}
