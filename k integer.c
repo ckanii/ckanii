e88d69ad88df7ec81e6c2aeead44db58672bb409
ckanii <35136096+ckanii@users.noreply.github.com>
2018-01-21 04:10:39 +0000

    Create k integer.c

---
+++ k integer.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,k,i;
+sum=0;
+scanf("%d%d,&n,&k);
+printf("%d%d,n,k");
+for(i=0;i<=n;i++)
+{
+printf("%d,"a[i]);
+}
+fori=1;i<=k;i++)
+{
+sum=sum+a[1];
+}
+printf("%d",sum);
+return0;
+}
