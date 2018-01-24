8ebdb1714bc32875d23d41e17aec1a7d46ba47af
ckanii <35136096+ckanii@users.noreply.github.com>
2018-01-24 16:43:34 +0530

    Create short.c

---
+++ short.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int array[];
+int a,b,num,temp;
+printf("enter the value of number \n);
+scanf("%d",&num);
+printf("%d",&num);
+printf("enter the elements one by one\n");
+for(i=0;i<=num;i++)
+{
+scanf("%d",array[a]);
+}
+printf("input array is\n");
+for(i=0;i<num;i++)
+{
+for(b=0;b<(num a-1);b++)
+{
+for(c=0;c<(num-a-1);c++)
+}
+}
+}
+
