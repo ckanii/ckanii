6b1d25b9ddebf7f1ee6312c7f2271fa5c9ce0f97
ckanii <35136096+ckanii@users.noreply.github.com>
2018-01-24 16:14:34 +0530

    Rename median.c to median n.c

---
+++ median n.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d[40],l;
+clrscr();
+p=o;
+t=0;
+k=m;
+p=(n+1)/2;
+for(i=0;i<=n-1;i++)
+{
+if(i==k)
+continue;
+else
+{
+if(d[k]<d[i])
+a++;
+else
+b++;
+}
+if(l==(l-1)&&b==l)
+printf("median is %dth location %d",k,d[k]);
+return 0;
+}
