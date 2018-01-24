f58da8287d5277c2e0be06d19c587ebdbe3cddcc
ckanii <35136096+ckanii@users.noreply.github.com>
2018-01-24 16:54:22 +0530

    Create hr.min.format.c

---
+++ hr.min.format.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int hours,minutes,total,minutes;
+printf("enter the time in hours and minutes:\n");
+printf("\n hours:\t");
+scanf("%d,&hours:\t");
+printf("\n minutes :\t");
+sacnf("%d,&minutes:\t");
+total minutes=(hours+60)=minutes;
+return 0;
+}
