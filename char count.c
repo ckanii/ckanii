261ad609007084f55c7226c851a620498b0bf5d2
ckanii <35136096+ckanii@users.noreply.github.com>
2018-02-02 15:55:09 +0530

    Create char count.c

---
+++ char count.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int char[i];
+string str="smart boy";
+printf("enter the character :");
+scanf("%d",&char[i]);
+str=strlen(smart boy);
+scanf("%s",&str);
+}
