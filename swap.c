41d108ee89f4361afe75343ac42ef1c1155bbcd9
ckanii <35136096+ckanii@users.noreply.github.com>
2018-02-05 16:14:32 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -4,12 +4,12 @@ int main()
     int x,y,temp;
     printf("enter the value of x&y\n");
     scanf("%d%d",&x,&y);
-    printf("before swapping \nx=%d\ny=%\n",x,y);
+    printf("before swapping \nx=%d\ny=%d\n",x,y);
     temp=x;
     x=y;
     y=temp;
     {
-    printf("after swapping \nx=%\ny=%\n",x,y);
+    printf("after swapping \nx=%d\ny=%d\n",x,y);
     return 0;
     }
 }
