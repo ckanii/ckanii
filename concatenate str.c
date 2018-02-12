23f427366533099db06e6b4e18dc9d8fb6d3ce97
ckanii <35136096+ckanii@users.noreply.github.com>
2018-02-12 16:28:24 +0530

    Create concatenate str.c

---
+++ concatenate str.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str1[20],str2[20],i,j;
+printf("enter the first string");
+scanf("%d",str1);
+printf("enter the second string");
+scanf("%d",str2);
+for(i=0;str1[i]!='\0';i++)
+for(i=o;str2[j]!='\0';i++)
+{
+str1[i]=str2[j];
+}
+str1[i]!='\0';
+printf("\n enter the output:%s");
+return 0;
+}
+
