35a71aab83688c9b4d5d0482e6bb488a67d529e1
ckanii <35136096+ckanii@users.noreply.github.com>
2018-02-12 16:08:52 +0530

    Create add str.c

---
+++ add str.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i;
+char str1[i],str2[i];
+printf("\n enter the two strings");
+gets[1];
+grt[2];
+i=0;
+while(str1[i]==str2[i]&&str1[i]!='\0')
+i++;
+if(str1>str2)
+printf("str1>str2");
+if else(str1<str2)
+printf("str1<str2")
+else
+printf("str1=str2");
+}
