2f12845f0980c444184251b1b08d13d1a00ed6a3
ckanii <35136096+ckanii@users.noreply.github.com>
2018-02-19 16:21:04 +0530

    Create n range min.c

---
+++ n range min.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("enter the number");
+scanf("%d",&i);
+n={"1,2,3,4,5,6,7,8,9,0"};
+if(n=i)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+return 0;
+}
+
+ 
