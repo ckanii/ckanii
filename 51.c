c9acff9ac83b91efa20927c3941ee654945845e5
ckanii <35136096+ckanii@users.noreply.github.com>
2018-02-26 15:54:44 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int n,rem=0,rev=0,count=0,i;
+   scanf("%d",&n);
+   while(n!='\0')
+   {
+       rem=n%10;
+       
+       rev=(rev*10)+rem;
+       n=n/10;
+}
+n=rev;
+
+while(n!='\0')
+{
+    rem=n%10;
+    printf("%d ",rem);
+    n=n/10;
+}
+getch();  
+}
