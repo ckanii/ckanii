ad69c50ee8782777d55991093d6df88b70e105f1
ckanii <35136096+ckanii@users.noreply.github.com>
2018-02-28 16:00:45 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,product;
+printf("enter the numbers");
+sum=a*b;
+scanf("%d%d",&a,&b);
+if(num%2==0)
+{
+printf("it is even number");
+}
+else
+{
+printf("it is odd number");
+}
+getch();
+}
