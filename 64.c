e9bcd69bd29e5ad5ce6e0ba9b30ba1fbf8d59333
ckanii <35136096+ckanii@users.noreply.github.com>
2018-03-12 16:22:40 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+printf("enter the numbers");
+scanf("%d %d",&a,&b);
+if(a%2==0)&&(b%2==0)
+{
+printf("even");
+}
+else
+{
+printf("odd");
+}
+return 0;
+}
