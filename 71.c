013ca7eaa9ded28a5e5bb25a7d30a8fbea59e535
ckanii <35136096+ckanii@users.noreply.github.com>
2018-03-19 07:09:23 -0400

    Create 71.c

---
+++ 71.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int c[100],d[100];
+int i;
+printf("enter the input");
+scanf("%s",&c);
+strcpy(d,c);
+strrev(c);
+i=strcmp(a,b);
+if(i==0)
+{
+printf("palindrome");
+}
+else
+{
+printf("not a palindrome");
+}
+return 0;
+}
